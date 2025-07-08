a8ea7c57401f0c5786abfbfcd2e775479bedf8fe
vinaythakoor004 <thakoorvinay04@gmail.com>
2025-07-08 15:09:11 +0530

    Added routes

--- intelligent-recipe-app-frontend/src/app/app.component.ts
+++ intelligent-recipe-app-frontend/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SearchComponent } from "./search/search.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SearchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
